a583f35e22a5a14e01f025708d65b0bff48d7a75
MatthieuNEDELEC <mnedelec@etud.univ-angers.fr>
2022-10-12 12:16:00 +0200

    Update Readme

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(expressLayouts);
 app.set('views', `${__dirname}/views`);
@@ -17,4 +17,4 @@ app.use((req, res, next) => {
     res.status(404).render('404', { page: 'Page not found' });
 });
 
-app.listen(port, console.log(`Server is listening at port ${port}.`));
+app.listen(port, console.log(`Le serveur est en marche sur le port ${port}.`));
