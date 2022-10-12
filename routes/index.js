bc234630fb512dcda3676408c6960331d41e5485
MatthieuNEDELEC <mnedelec@etud.univ-angers.fr>
2022-10-12 12:16:33 +0200

    update README

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { page: 'Home' });
+  res.render('accueil', { page: 'Home' });
 });
 
 router.get('/about', (req, res, next) => {
